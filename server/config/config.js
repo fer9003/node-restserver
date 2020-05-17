6a426ea6b7b1b751661b24d71d8b5a1f65faff3b
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-05-17 17:27:36 -0500

    Rest Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,18 @@
 // Puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://f90mora:ubVtzDnGY9pLgCv0@cluster0-h4tx0.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
