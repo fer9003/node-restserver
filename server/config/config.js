0c470b673b422b1f1740fdb73e590d8faa9a4234
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-05-17 17:52:12 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://f90mora:ubVtzDnGY9pLgCv0@cluster0-h4tx0.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
