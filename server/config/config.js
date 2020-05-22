466010025bc29a7a4c654496e72653190b89d1f5
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-05-21 21:25:56 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,14 @@ process.env.PORT = process.env.PORT || 3000;
 //ENTORNO
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
+
+//VENCIMIENTO DEL TOKEN
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//SEED DE AUTENTICACION
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
+
 //BASE DE DATOS
 let urlDB;
 
--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+
+//Configuracion global de rutas
+app.use(require('./routes/index'));
+
 
 moongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, (err, res) => {
     if (err) throw err;
